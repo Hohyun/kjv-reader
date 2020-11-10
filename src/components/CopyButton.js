acaa882ae2f4905a779e0431e65a603eda0edf05
Hohyun Kim <skykim63@gmail.com>
2020-11-10 13:03:47 +0900

    Change CopyButton into functional component

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -1,25 +1,18 @@
 import React from 'react'
 
-class CopyButton extends React.Component {
-    onButtonClick = (lang) => {
-        this.props.onClick(lang);
-    }
-    
-    render() {
-        return (
-            <React.Fragment>
-                <button  
-                    className="ui secondary basic button"
-                    onClick={() => this.onButtonClick(this.props.lang)} 
-                >
-                    <i className="copy icon"></i>
-                    Copy
-                </button>
-                <div className="ui horizontal label">{this.props.msg}</div>
-            </React.Fragment>
-        );
-    }
+const CopyButton = (props) => {
+    return (
+        <React.Fragment>
+            <button
+                className="ui secondary basic button"
+                onClick={() => props.onClick(props.lang)}
+            >
+                <i className="copy icon"></i>
+                Copy
+            </button>
+            <div className="ui horizontal label">{props.msg}</div>
+        </React.Fragment>
+    );
 }
 
 export default CopyButton;
-
